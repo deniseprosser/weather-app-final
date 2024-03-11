61b6d5e4c770835821e3df828bf0094aed34f368
deniseprosser <denise.prosser23@gmail.com>
2024-03-11 13:27:03 +0000

    Added search functionality with API integration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-function searchCity(event) {
+function displayWeather(response) {
+  let temperatureValueElement = document.querySelector("#temperature-value");
+  let temperature = response.data.temperature.current;
+  let city = document.querySelector("#current-city");
+  city.innerHTML = response.data.city;
+  temperatureValueElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "aab3d3obc0205ebt561454f09dff85ec";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-input");
-  let city = document.querySelector("#current-city");
-  city.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchCity);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("London");
