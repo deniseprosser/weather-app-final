f5c54e62919bcbc176ea9c80395aee2e0c9c74d1
deniseprosser <denise.prosser23@gmail.com>
2024-03-11 14:38:00 +0000

    Added more weather data

--- src/index.js
+++ src/index.js
@@ -2,8 +2,41 @@ function displayWeather(response) {
   let temperatureValueElement = document.querySelector("#temperature-value");
   let temperature = response.data.temperature.current;
   let city = document.querySelector("#current-city");
+  let conditionsElement = document.querySelector("#conditions");
+  let humidityElement = document.querySelector("#humidity-value");
+  let windElement = document.querySelector("#wind-speed-value");
+  let dateElement = document.querySelector("#current-date");
+  let date = new Date(response.data.time * 1000);
+
   city.innerHTML = response.data.city;
+  conditionsElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureValueElement.innerHTML = Math.round(temperature);
+  dateElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -15,7 +48,6 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-input");
-
   searchCity(searchInput.value);
 }
 
