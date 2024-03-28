01e100241ac7ef519d72efc5bb251087c5fafb5e
deniseprosser <denise.prosser23@gmail.com>
2024-03-28 11:11:33 +0000

    Forecast HTML added from JavaScript

--- src/index.js
+++ src/index.js
@@ -56,7 +56,28 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-item">
+            <div class="weather-forcast-day">${day}</div>
+            <div class="weather-forecast-icon">☀</div>
+            <div class="weather-forecast-temp">
+              <span class="forecast-high"><strong>18</strong></span>
+              <span class="forecast-low">10</span>
+            </div>
+          </div>`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("London");
+displayForecast();
